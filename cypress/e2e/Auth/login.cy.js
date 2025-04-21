5d5d88747cb4753e7d7df050cea271f511f5fe17
SEVIMA-212\Sevima 212 <rizvifeby@gmail.com>
2025-04-21 10:17:39 +0700

    Update

--- cypress/e2e/Auth/login.cy.js
+++ cypress/e2e/Auth/login.cy.js
@@ -10,13 +10,5 @@ describe ('Login',() => {
         Page.login.password(Cypress.env('password'))
         Page.login.loginButton()
         Page.login.loginAssertion(login_assertion.Success_Login[0,1])
-        // cy.get('button[type="submit"]').click() 
-        // cy.url().should('include', '/dashboard') 
-        // cy.get('.welcome-message').should('contain', 'Welcome') 
     })
-    // it('Logout', () => {
-    //     cy.get('.logout-button').click() 
-    //     cy.url().should('include', '/login') 
-    //     cy.get('.login-message').should('contain', 'Please log in') 
-    // })  
 })
